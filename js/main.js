07e837b4e5a902576932cea89449a42c30458f80
Rahman Are <rahman@softcom.ng>
2020-05-19 18:44:08 +0100

    Related

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// 
-// Handling the menu toggle
-// 
-
-var mobileNav = document.querySelector(".mobile-nav");
-function showmenu(x) {
-    x.classList.toggle("change");
-    mobileNav.classList.toggle("open-nav");
-}
